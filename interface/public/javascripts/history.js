f07532b2aa46d15ad90dfc9518a78be0b404c82d
noobgab <lolgab2010@yahoo.ie>
2018-11-29 21:03:15 +0000

    testing charts in the history page

--- interface/public/javascripts/history.js
+++ interface/public/javascripts/history.js
@@ -1,38 +1,12 @@
 $(document).ready(function() {
     var ctx = document.getElementById("historicDataChart").getContext('2d');
+
+    var data = [{x:10, y:20}, {x:20, y:12}, {x:30, y:6}, {x:40, y:16}, {x:50, y:23}];
+    var options = {label: "Humidity"};
+
     var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        }, options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+        type: 'line',
+        data: data,
+        options: options
     });
 });
