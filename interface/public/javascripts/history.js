5ff1ec4f18b553f76a8f42480273b587ea1cd6ba
noobgab <lolgab2010@yahoo.ie>
2018-11-29 21:04:16 +0000

    testing charts in the history page

--- interface/public/javascripts/history.js
+++ interface/public/javascripts/history.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     var ctx = document.getElementById("historicDataChart").getContext('2d');
-
-    var data = [{x:10, y:20}, {x:20, y:12}, {x:30, y:6}, {x:40, y:16}, {x:50, y:23}];
-    var options = {label: "Humidity"};
-
     var myChart = new Chart(ctx, {
         type: 'line',
-        data: data,
-        options: options
+        data: [{
+        x: 10,
+        y: 20
+    }, {
+        x: 15,
+        y: 10
+    }]
     });
 });
