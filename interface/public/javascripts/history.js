d3c9c7060f87d4fadd99190fa93f72ee1c92c6fc
noobgab <lolgab2010@yahoo.ie>
2018-11-29 21:12:33 +0000

    testing charts in the history page

--- interface/public/javascripts/history.js
+++ interface/public/javascripts/history.js
@@ -5,8 +5,11 @@ $(document).ready(function() {
         data: {
             labels: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
             datasets: [{
-                label: "2015",
+                label: "Humidity",
                 data: [10,8,6,5,12,8,16,17,6,7,6,10]
+            }, {
+                label: "Temperature",
+                data: [10,6,7,6,17,16,8,12,5,6,8,10]
             }]
         }
     });
